3e3722e78c7f94eda5d60744a58f8c1394028fdf
Eduardo Morales <Eduardomorales.dev@gmail.com>
2021-02-18 13:11:29 -0700

    Added project info

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Portfolio - Josemoralesdev`,
+    description: `Personal portfolio page`,
+    author: `@josemorales.dev`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -19,8 +20,8 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
+        short_name: `portfolio`,
+        start_url: `/index.js`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
