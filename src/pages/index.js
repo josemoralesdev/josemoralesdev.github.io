4e0d92a78d7ea0ee836c0047f16590513f9b05e4
Eduardo Morales <Eduardomorales.dev@gmail.com>
2021-02-18 13:25:52 -0700

    Added imports for component rendering

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,27 @@
 import React from "react"
 import { Link } from "gatsby"
+import Zoom from 'react-reveal/Zoom'
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
-
+import Header from '../components/header/'
+import HomeSection from "../components/home-section"
+import About from "../components/about"
+import Services from "../components/services"
+import Portfolio from "../components/portfolio"
+import Contact from "../components/contact"
+import Footer from '../components/footer'
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
+  <>
+    <Header />
+    <HomeSection />
+    <About />
+    <Services />
+    <Portfolio />
+    <Contact />
+    <Footer />
+  </>
 )
 
 export default IndexPage
