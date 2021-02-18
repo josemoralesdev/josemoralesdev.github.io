d87cc0d0150d3b223b8ea1318cb78f5febce60a8
Eduardo Morales <Eduardomorales.dev@gmail.com>
2021-02-18 13:19:22 -0700

    Created card component for service section & added styling

---
+++ src/components/card/index.jsx
@@ -0,0 +1,47 @@
+import React from "react"
+import style from "./index.module.scss"
+
+export default function Card({
+  iconsrc,
+  headerTitle,
+  headerText,
+  descriptionTitle,
+  descriptionText,
+  toolsTitle,
+  toolsText,
+}) {
+  const toolsList = toolsText.split(",").map(tool => {
+    return (
+      <p className={style.text} key={tool}>
+        {tool}
+      </p>
+    )
+  })
+  const descriptionList = descriptionText.split(",").map(item => {
+    return (
+      <p className={style.text} key={item}>
+        {item}
+      </p>
+    )
+  })
+
+  return (
+    <>
+      <div className={style.card}>
+        <div className={style.cardHeader}>
+          <img src={iconsrc} alt="" srcSet="" />
+          <p className={`${style.title} `}>{headerTitle}</p>
+          <p className={style.text}>{headerText}</p>
+        </div>
+        <div className={style.cardDescription}>
+          <p className={`${style.title} `}>{descriptionTitle}</p>
+          {descriptionList}
+        </div>
+        <div className={style.cardTools}>
+          <p className={`${style.title} `}>{toolsTitle}</p>
+          {toolsList}
+        </div>
+      </div>
+    </>
+  )
+}
