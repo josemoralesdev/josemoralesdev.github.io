941b09ebe65118efd13037ef736f5609683b9296
Eduardo Morales <Eduardomorales.dev@gmail.com>
2021-03-05 14:25:55 -0700

    Added gatsby image query to index.jsx, added margin center on .wrapper

--- src/components/home-section/index.jsx
+++ src/components/home-section/index.jsx
@@ -1,24 +1,48 @@
 import React from "react"
 import { HomeData } from "../../data/config.js"
-import ProfilePicture from "../../assets/images/profile-picture.png"
 import Styles from "./index.module.scss"
 import SocialList from "../social-list"
 import Button from "../../common/button"
+import Resume from "../../assets/files/Resume.pdf"
+import Img from "gatsby-image"
+import { graphql, useStaticQuery } from "gatsby"
 
 export default function HomeSection() {
+  const data = useStaticQuery(graphql`
+    query ProfilePicture {
+      file(relativePath: { eq: "profile-picture.png" }) {
+        id
+        childImageSharp {
+          fixed(height: 300, width: 300) {
+            ...GatsbyImageSharpFixed
+          }
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
   const { greeting, role, defaultAction, callToAction } = HomeData
   return (
     <>
       <section id={HomeData.section} className={Styles.homeSection}>
         <div className={Styles.wrapper}>
-          <img src={ProfilePicture} className={Styles.profilePicture} alt="" />
+          <Img
+            fixed={data.file.childImageSharp.fixed}
+            className={Styles.profilePicture}
+            srcSet=""
+            alt="José E. Profile Picture"
+          />
           <p className={Styles.greeting}>{greeting}</p>
           <p className={Styles.role}>{role}</p>
           <div className={Styles.buttonRow}>
             <a href="#about">
               <Button type="default" text={defaultAction} />
             </a>
-            <Button type="primary" text={callToAction} />
+            <a href={Resume} download target="_blank">
+              <Button type="primary" text={callToAction} />
+            </a>
           </div>
           <SocialList />
         </div>
