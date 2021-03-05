18c64a4aab7a239491331324978772d5452b5e5b
Eduardo Morales <Eduardomorales.dev@gmail.com>
2021-03-05 14:28:09 -0700

    Added gatsby image query to index.jsx, added fluid image to about section.

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -2,13 +2,35 @@ import React from "react"
 import { AboutData } from "../../data/config"
 import Banner from "../banner/banner.jsx"
 import styles from "./index.module.scss"
+import { graphql, useStaticQuery } from "gatsby"
+import Img from "gatsby-image"
 export default function About() {
-  const { section, image, textOne, textTwo, textThree } = AboutData
+  const data = useStaticQuery(graphql`
+    query aboutImage {
+      file(relativePath: { eq: "create.png" }) {
+        id
+        childImageSharp {
+          fixed(height: 300, width: 300) {
+            ...GatsbyImageSharpFixed
+          }
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+  const { section, textOne, textTwo, textThree } = AboutData
   return (
     <>
       <section className={section} id={section}>
         <Banner text="About Me" direction="down" position="right" />
-        <img src={image} alt="" srcSet="" className={styles.image} />
+        <Img
+          fluid={{ ...data.file.childImageSharp.fluid, aspectRatio: 1 }}
+          alt=""
+          srcSet=""
+          className={styles.image}
+        />
         <p className={styles.aboutText}>{textOne}</p>
         <p className={styles.aboutText}>{textTwo}</p>
         <p className={styles.aboutText}>{textThree}</p>
