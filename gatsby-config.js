c69fcc07fd469c5e9b062fb8a3e09c5ba70c669a
Eduardo Morales <Eduardomorales.dev@gmail.com>
2021-03-05 14:20:36 -0700

    Corrected gatsby image folder path

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+
+})
 module.exports = {
   siteMetadata: {
     title: `Portfolio - Josemoralesdev`,
@@ -11,7 +15,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/src/assets/images`,
       },
     },
     `gatsby-transformer-sharp`,
