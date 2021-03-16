7f613161db9b61ef0cb2005c456616a905d5200a
Eduardo Morales <Eduardomorales.dev@gmail.com>
2021-03-15 19:35:30 -0700

    Modified graphql query for fluid img, added styling on classNames & added columns left & right (divs)

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -8,13 +8,14 @@ export default function About() {
   const data = useStaticQuery(graphql`
     query aboutImage {
       file(relativePath: { eq: "create.png" }) {
-        id
         childImageSharp {
-          fixed(height: 300, width: 300) {
-            ...GatsbyImageSharpFixed
-          }
-          fluid {
+          fluid(
+            maxWidth: 500
+            maxHeight: 500
+            srcSetBreakpoints: [250, 350, 450, 500]
+          ) {
             ...GatsbyImageSharpFluid
+            ...GatsbyImageSharpFluidLimitPresentationSize
           }
         }
       }
@@ -23,17 +24,21 @@ export default function About() {
   const { section, textOne, textTwo, textThree } = AboutData
   return (
     <>
-      <section className={section} id={section}>
+      <section className={styles.about} id={section}>
         <Banner text="About Me" direction="down" position="right" />
-        <Img
-          fluid={{ ...data.file.childImageSharp.fluid, aspectRatio: 1 }}
-          alt=""
-          srcSet=""
-          className={styles.image}
-        />
-        <p className={styles.aboutText}>{textOne}</p>
-        <p className={styles.aboutText}>{textTwo}</p>
-        <p className={styles.aboutText}>{textThree}</p>
+        <div className={styles.leftColumn}>
+          <Img
+            fluid={{ ...data.file.childImageSharp.fluid, aspectRatio: 1 }}
+            alt=""
+            srcSet=""
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.rightColumn}>
+          <p className={styles.aboutText}>{textOne}</p>
+          <p className={styles.aboutText}>{textTwo}</p>
+          <p className={styles.aboutText}>{textThree}</p>
+        </div>
       </section>
     </>
   )
