a811fd71beedd285873498fcb416662a207edb5a
Eduardo Morales <Eduardomorales.dev@gmail.com>
2021-03-17 18:17:42 -0700

    Added Fade effect to component.

--- src/components/banner/banner.jsx
+++ src/components/banner/banner.jsx
@@ -1,17 +1,20 @@
 import React from "react"
 import styles from "./banner.module.scss"
+import Fade from "react-reveal"
 
 export default function Banner({ text, direction, position }) {
   const getDirection = direction === "up" ? styles.up : styles.down
   const getPosition = position === "right" ? styles.right : styles.left
   return (
     <>
-      <div
-        className={`${styles.background} ${getDirection} ${getPosition}`}
-        id="background"
-      >
-        <span>{text}</span>
-      </div>
+      <Fade bottom right cascade>
+        <div
+          className={`${styles.background} ${getDirection} ${getPosition}`}
+          id="background"
+        >
+          <span>{text}</span>
+        </div>
+      </Fade>
     </>
   )
 }
