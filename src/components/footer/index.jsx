e586bf85dcbcd30bae4b34d6d6637acf199a36de
Eduardo Morales <Eduardomorales.dev@gmail.com>
2021-03-17 18:23:51 -0700

    Commented unused components, added styling for navLinks.

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,19 +1,19 @@
 import React from "react"
-import { HeaderData } from "../../data/config"
 import { FooterData } from "../../data/config"
-import NavLinks from "../nav-links"
-import Logo from "../logo"
 import styles from "./index.module.scss"
-import SocialList from "../social-list"
+//import NavLinks from "../nav-links"
+//import Logo from "../logo"
 
 export default function Footer() {
-  const { navLinks, logo } = HeaderData
+  //const { navLinks, logo } = HeaderData
   const { copyright } = FooterData
   return (
     <>
       <footer className={styles.footer}>
-        <Logo className={styles.logo} inverted="true" />
-        <NavLinks className={styles.navLinks} />
+        {/*<Logo className={styles.logo} inverted="true" />*/}
+        {/*<ul className={styles.navLinks}>
+          <NavLinks className="" />
+  </ul>*/}
         <p>{copyright}</p>
       </footer>
     </>
