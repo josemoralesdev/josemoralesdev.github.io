b7309d7954d662a851ff0e48e39b2acc3a648ddc
Eduardo Morales <Eduardomorales.dev@gmail.com>
2021-05-25 22:51:56 -0700

    Removed unused variable

--- src/components/nav-links/index.jsx
+++ src/components/nav-links/index.jsx
@@ -1,6 +1,5 @@
 import React from "react"
 import { HeaderData } from "../../data/config"
-import styles from "./index.module.scss"
 export default function NavLinks({ className }) {
   const { navLinks } = HeaderData
   const navLinksList = navLinks.map(links => {
