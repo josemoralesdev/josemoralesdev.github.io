2fbd0e6dbfb2d67343af78c18bf582401c969a1a
Eduardo Morales <Eduardomorales.dev@gmail.com>
2021-05-25 20:44:32 -0700

    Added reusable layout component.

---
+++ src/components/layout/index.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Footer from "../footer"
+import Header from "../header"
+
+export const Layout = ({ children }) => {
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  )
+}
