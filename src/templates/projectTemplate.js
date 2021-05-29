ac98ef94f739a6488f9def70489a37fd183e83c4
Eduardo Morales <Eduardomorales.dev@gmail.com>
2021-05-28 22:30:50 -0700

    Tweaked styling.

--- src/templates/projectTemplate.js
+++ src/templates/projectTemplate.js
@@ -11,18 +11,18 @@ export default function Template({ data }) {
 
   const formatDate = (date) => {
     const dateString = new Date(date);
-    let day = dateString.getDay();
+    let month = dateString.getMonth();
     let year = dateString.getFullYear();
 
-    return `${day}/${year}`
+    return `${month}/${year}`
   }
   return (
     <Layout>
       <SEO title={title} />
       <section style={{ minHeight: "100vh", maxWidth: "1024px" }}>
-        <div dangerouslySetInnerHTML={{ __html: html }} />
-        <div>Author: {author}</div>
-        <div>Date: {formatDate(date)}</div>
+        <div dangerouslySetInnerHTML={{ __html: html }} style={{ padding: "4em 0 0" }} />
+        {/* <div>Author - {author}</div> */}
+        <small style={{ padding: "0 0 2em", fontStyle: "italic", display: "block" }}>{formatDate(date)}</small>
         <Link to="/#projects">
           <Button text="Go back" type="primary" isCentered="yes" />
         </Link>
