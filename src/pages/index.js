821a75cf7f1072603c2f5f738cfec2bb51b67f68
Eduardo Morales <Eduardomorales.dev@gmail.com>
2021-05-29 18:39:23 -0700

    Imported themeprovider & project theme.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,28 @@
 import React from "react"
 import SEO from "../components/seo"
 import { Home } from "../components/home"
-import About from "../components/about"
+import { About } from "../components/about"
 import Services from "../components/services"
 import { Portfolio } from "../components/portfolio"
 import { Contact } from "../components/contact"
 import { Layout } from '../components/layout'
 import { GlobalStyle } from "../theme/globalStyles"
+import { ThemeProvider } from "styled-components"
+import { theme } from "../theme"
+
 const IndexPage = () => (
   <>
     <SEO title="Home" />
-    <Layout>
-      <GlobalStyle />
-      <Home />
-      <About />
-      <Services />
-      <Portfolio />
-      <Contact />
-    </Layout>
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <GlobalStyle />
+        <Home />
+        <About />
+        <Services />
+        <Portfolio />
+        <Contact />
+      </Layout>
+    </ThemeProvider>
   </>
 )
 
