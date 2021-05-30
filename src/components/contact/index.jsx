a376048574e645e129e05e1d43f6e0c91ab397b6
Eduardo Morales <Eduardomorales.dev@gmail.com>
2021-05-29 22:34:09 -0700

    Migrated contact section to styled.

--- src/components/contact/index.jsx
+++ src/components/contact/index.jsx
@@ -3,28 +3,77 @@ import { ContactData } from "../../data/config"
 import Banner from "../banner/banner"
 import style from "./index.module.scss"
 import Form from "../form"
+import styled from "styled-components"
+import { Section } from "../../common/styles/section"
+import { devices } from "../../theme/breakpoints"
+
+const ContactSection = styled(Section)`
+  @media${devices.tablet} {
+    display: flex;
+    flex-flow: row wrap;
+  }
+`
+const ContactTitle = styled.h2`
+  max-width: 80%;
+`
+const ContactSubtitle = styled.p`
+  font-weight: 300;
+  font-size: 1.71em;
+  letter-spacing: var(--letter-spacing);
+  text-align: justify;
+  margin: 50px auto 0;
+  width: 80%;
+  @media${devices.tablet} {
+    text-align: center;
+    width: 80%;
+  }
+`
+const LeftColumn = styled.div`
+  display: flex;
+  @media ${devices.tablet} {
+    margin-top: 100px;
+    flex: 1 1 100%;
+  }
+`
+const RightColumn = styled.div`
+  @media ${devices.tablet} {
+    margin-top: 100px;
+    flex: 1 1 100%;
+  }
+`
+const Illustration = styled.img`
+  width: 80%;
+  height: auto;
+  margin: 50px auto;
+  max-width: 500px;
+  @media ${devices.tablet} {
+    max-width: 40%;
+    align-self: flex-start;
+    margin: 0 auto;
+  }
+`
 
 export const Contact = () => {
   const { section, title, subtitle, illustration } = ContactData
   return (
     <>
-      <section id={section} className={style.contact}>
+      <ContactSection id={section}>
         <Banner direction="up" position="left" text="contact" />
-        <h2 className={style.title}>{title}</h2>
-        <p className={style.subtitle}>{subtitle}</p>
-        <div className={style.leftColumn}>
-          <img
+        <ContactTitle>{title}</ContactTitle>
+        <ContactSubtitle>{subtitle}</ContactSubtitle>
+        <LeftColumn>
+          <Illustration
             className={style.illustration}
             src={illustration}
             width="300px"
             alt=""
             srcSet=""
           />
-        </div>
-        <div className={style.rightColumn}>
+        </LeftColumn>
+        <RightColumn>
           <Form />
-        </div>
-      </section>
+        </RightColumn>
+      </ContactSection>
     </>
   )
 }
