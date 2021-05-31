dfea459793cb79a92c21a3f351f57b0083d165ea
Eduardo Morales <Eduardomorales.dev@gmail.com>
2021-05-31 15:53:22 -0700

    Modified maxwidth, fontVariant.

--- src/common/styles/section.jsx
+++ src/common/styles/section.jsx
@@ -2,12 +2,11 @@ import styled from "styled-components"
 import { devices } from "../../theme/breakpoints"
 
 export const Section = styled.section`
-  text-transform: lowercase;
   font-family: Roboto, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
     Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-  font-variant: small-caps;
+  font-variant: all-small-caps;
   margin: 0 auto 5.769em;
-  max-width: ${props => props.theme.maxWidth};
+  max-width: ${props => props.theme.globals.maxWidth};
   padding: 5.769em 16px;
   @media ${devices.tablet} {
     padding: 95px 2rem;
