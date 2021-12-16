5162ca0b377fb854acba85d1d718f5c9e433259a
Eduardo Morales <Eduardomorales.dev@gmail.com>
2021-12-15 21:29:20 -0700

    Added JEM logo, removed gatsby-icon file.

--- gatsby-config.js
+++ gatsby-config.js
@@ -87,7 +87,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#3f3f3f`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/JEM_logo_maskable.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
